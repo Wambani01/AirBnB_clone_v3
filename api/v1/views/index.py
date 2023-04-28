ac5f70bf47301a571e56e152605d44e9b79f339a
Wambani01 <georgewmbn@gmail.com>
2023-04-28 12:47:03 -0700

    changed imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,14 +3,8 @@
 """index file for flask app"""
 
 from api.v1.views import app_views
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
