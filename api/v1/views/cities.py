fc3c17999ace3533b8bafa8a64d3e69dca95ab35
Wambani01 <georgewmbn@gmail.com>
2023-04-29 05:29:45 -0700

    edited cities file

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,6 +8,7 @@ from models.state import State
 from models import storage
 from models.city import City
 
+
 @app_views.route('/states/<state_id>/cities',
                  methods=['GET', 'POST'], strict_slashes=False)
 def cities_by_state(state_id):
@@ -32,14 +33,15 @@ def cities_by_state(state_id):
                 if my_dict.get("name") is None:
                     abort(400, 'Missing name')
                 my_dict["state_id"] = state_id
-                new_city = City(**my_dict)
-                new_city.save()
-                return jsonify(new_city.to_dict()), 201
+                city = City(**my_dict)
+                city.save()
+                return jsonify(city.to_dict()), 201
 
 
-@app_views.route('/api/v1/cities/<city_id>',
+@app_views.route('/cities/<string:city_id>',
                  methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def city_by_city_id(city_id):
+    """retrieves cities by cities id"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -48,10 +50,11 @@ def city_by_city_id(city_id):
     elif request.method == 'DELETE':
         storage.delete(city)
         storage.save()
+        return jsonify({}), 200
     elif request.method == 'PUT':
         my_dict = request.get_json()
         if my_dict is None:
             abort(400, 'Not a JSON')
-        new_city.name = my_dict.get("name")
-        new_city.save()
-        return jsonify(new_city.to_dict()), 200
+        city.name = my_dict.get("name")
+        city.save()
+        return jsonify(city.to_dict()), 200
